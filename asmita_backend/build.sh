0a40ad3425256deb157f04edd99f160d1fb5d91e
Huzaifa Chouhan <huzefachouhan07@gmail.com>
2026-09-16 12:09:05 +0530

    solved the deployment problem

--- asmita_backend/build.sh
+++ asmita_backend/build.sh
@@ -1,6 +1,13 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Upgrade pip and install requirements
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Convert static asset files
 python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
 python manage.py migrate
